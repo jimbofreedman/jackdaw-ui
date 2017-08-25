f9bdf9e309cf959c1cd7881627e5ca281c1ffba0
Jimbo Freedman <github@jlf.me>
2017-08-25 12:54:35 +0100

    Moving map

--- app/components/LeafletMap/index.js
+++ app/components/LeafletMap/index.js
@@ -11,19 +11,44 @@ import LeafletCss from 'leaflet/dist/leaflet.css'; // eslint-disable-line no-unu
 import CustomLeafletCss from './leaflet.css'; // eslint-disable-line no-unused-vars
 import config from '../../config';
 
-function LeafletMap() {
-  const loc = [51.4665195, -0.1943263];
-  return (
-    <div>
-      <Map cssClass="leaflet-touch" style={{ height: '275px', width: '750px' }} center={loc} zoom={13}>
-        <TileLayer
-          url={`${config.tileEndpoint}/osm_tiles/{z}/{x}/{y}.png`}
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        />
-        <CircleMarker center={loc} radius={10} />
-      </Map>
-    </div>
-  );
+export class LeafletMap extends React.PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      position: [0, 0],
+    };
+
+    this.updatePosition = this.updatePosition.bind(this);
+  }
+
+  componentDidMount() {
+    if (navigator.geolocation) {
+      navigator.geolocation.watchPosition(this.updatePosition);
+    } else {
+      console.log('Geolocation not supported');
+    }
+  }
+
+  updatePosition(position) {
+    console.log(position);
+    this.setState({ position: [position.coords.latitude, position.coords.longitude] });
+  }
+
+  render() {
+    const { position } = this.state;
+    return (
+      <div>
+        <Map cssClass="leaflet-touch" style={{ height: '275px', width: '750px' }} center={position} zoom={13}>
+          <TileLayer
+            url={`${config.tileEndpoint}/osm_tiles/{z}/{x}/{y}.png`}
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          />
+          <CircleMarker center={position} radius={10} />
+        </Map>
+      </div>
+    );
+  }
 }
 
 LeafletMap.propTypes = {
